012674b004cbf0410a30ba3064eb491ca8375899
Xue Hu <xhu@lendingclub.com>
2019-01-08 00:47:14 -0800

    5 omg can't read prompt for LENGTH

--- 5/5.js
+++ 5/5.js
@@ -1,14 +1,15 @@
-// const {readInput} = require('../util');
-// const input5 = readInput('input5.txt');
+const {readInput} = require('../util');
+let input5 = readInput('input5.txt');
 
 const http = require('http')
 const port = 3000
 
 const requestHandler = (request, response) => {
   console.log(request.url)
-debugger
-  const res = getResultingPolymer(test);
-  response.end(res)
+
+  input5 = input5[0];
+  const res = `A: ${getResultingPolymerLen(input5)}\nB: ${getBestResultingPolymerLenIfRemove(input5)}`
+  response.end(res);
 }
 
 const server = http.createServer(requestHandler)
@@ -21,26 +22,51 @@ server.listen(port, (err) => {
   console.log(`server is listening on ${port}`)
 })
 
-const getResultingPolymer = (input) => {
+const isReactive = (c1, c2) => {
+  if (typeof c1 !== 'string' || typeof c2 !== 'string') return;
+  if (c1.toLowerCase() === c2.toLowerCase() && (c1.toUpperCase() === c1 && c2.toLowerCase() === c2 || c1.toLowerCase() === c1 && c2.toUpperCase() === c2)) return true;
+}
+
+const getBestResultingPolymerLenIfRemove = (input) => {
+  const defaultBest = input.length;
+  return 'abcdefghijklmnopqrstuvwxyz'.split('').reduce((best, char) => {
+    let len = getResultingPolymerLen(input.replace(new RegExp(char, 'gi'), ''));
+    return len < best ? len : best;
+  }, defaultBest);
+}
+
+const getResultingPolymerLen = (input) => {
   input = input.split('');
-  const reactiveCombos = new Set(['aA', 'Aa', 'bB', 'Bb', 'cC', 'Cc']);
   const len = input.length;
-  let i = j = 0;
-  while (i < len-1) {
-    let index = i-j, index2 = i+1+j
-    index = index > 0 ? index : i;
-    index2 = index2 < len ? index2 : i+1;
-    if (reactiveCombos.has(input[index] + input[index2])) {
-      input[index] = null;
-      input[index2] = null;
+  let i = 0, j = 1;
+  let lastIterWasMatch = false;
+  while (i < len && j < len) {
+    while (isReactive(input[i], input[j])) {
+      input[i] = null;
+      input[j] = null;
+
+      // TODO keep track of the last i of char searching backwards instead of this while loop. would cover the if case too...
+      // or probably just splice, jeez
+      // i = indexOfLastCharSearchingBackwards;
+
+      while (typeof input[i] !== 'string' && i > -1) i--;
+      if (i === 0 && typeof input[0] !== 'string') {
+        i = j;
+      }
+
       j++;
+      lastIterWasMatch = true; // went into this loop
     }
-    i++;
-    debugger
+    if (lastIterWasMatch) {
+      i = j;
+      j = i+1;
+    } else {
+      i++; j++;
+    }
+    lastIterWasMatch = false;
   }
-  return 'haha';
+  return input.filter(c => typeof c !== 'undefined').join('').length;
 }
 
 const test = 'dabAcCaCBAcCcaDA';
 const testExpected = 'dabCBAcaDA';
-// makeRequest(getResultingPolymer(test));
